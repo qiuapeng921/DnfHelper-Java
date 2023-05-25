653e69fbd615bbab1bafa149cc6ace2e692bac8f
chenzhangyue <chenzhangyue01@weidian.com>
2023-05-25 17:53:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# auto-dnf
+
+[![GitHub license](https://img.shields.io/badge/MIT_License-blue.svg)](https://raw.githubusercontent.com/lunasaw/luna-mt-doc/master/LICENSE)
+
+[www.isluna.ml](http://www.isluna.ml) 
---
+++ src/main/java/io/github/lunasaw/Main.java
@@ -0,0 +1,11 @@
+package io.github.lunasaw;
+
+/**
+ * @author weidian
+ * @date 2023/5/25
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
