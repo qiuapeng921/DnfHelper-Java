9ac74792f4d7a888f1a3a52482982bf002714eab
qiuapeng <qiuapeng@vchangyi.com>
2023-05-26 13:37:07 +0800

    添加常用方法

--- src/main/java/dnf/Main.java
+++ src/main/java/dnf/Main.java
@@ -1,14 +1,15 @@
 package dnf;
 
+import dnf.helper.Bytes;
+import dnf.helper.Process;
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * @author lunasaw
- * @date 2023/5/25
- */
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        log.info("main::args = {}", args);
+        byte[] bytes1 = Bytes.intToByteArray(123);
+        log.info("main::args = {}", bytes1);
+        byte[] bytes = Process.readMemory(6976,64562121L, 10);
+        log.info("main::args = {}", bytes);
     }
 }
--- src/main/java/dnf/helper/Process.java
+++ src/main/java/dnf/helper/Process.java
@@ -1,22 +1,60 @@
 package dnf.helper;
+import com.sun.jna.Memory;
+import com.sun.jna.Pointer;
 import com.sun.jna.platform.win32.Kernel32;
 import com.sun.jna.platform.win32.WinNT;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Process {
-
     public static WinNT.HANDLE openProcess(Integer pid){
         Kernel32 kernel32 = Kernel32.INSTANCE;
-        WinNT.HANDLE process = kernel32.OpenProcess(0x1F0FFF, false, pid);
-        if (process == null) {
+        WinNT.HANDLE handle = kernel32.OpenProcess(0x1F0FFF, false, pid);
+        if (handle == null){
             int error = kernel32.GetLastError();
             log.error("openProcess::pid = {} , error = {}", pid, error);
-        } else {
-            log.info("Process opened successfully = {}", pid);
-            kernel32.CloseHandle(process);
+            return null;
         }
-        return process;
+
+        return handle;
     }
 
+    public static byte[] readMemory(Integer processId, long address, int size) {
+        Kernel32 kernel32 = Kernel32.INSTANCE;
+        WinNT.HANDLE handle = openProcess(processId);
+        if (handle == null){
+            return null;
+        }
+
+        Memory buffer = new Memory(size);
+        boolean result = kernel32.ReadProcessMemory(handle, new Pointer(address), buffer, size, null);
+        if (!result) {
+            int error = kernel32.GetLastError();
+            log.error("readMemory:: address = {} , error = {}", address, error);
+            return null;
+        }
+
+        return buffer.getByteArray(0,size);
+    }
+
+    public static boolean writeMemory(Integer processId, long address, byte[] data) {
+        Kernel32 kernel32 = Kernel32.INSTANCE;
+        WinNT.HANDLE handle = openProcess(processId);
+        if (handle == null){
+            return false;
+        }
+
+
+        int size = data.length;
+        Memory buffer = new Memory(size);
+        buffer.write(0, data, 0, size);
+        boolean result = kernel32.WriteProcessMemory(handle, new Pointer(address), buffer, size, null);
+        if (!result) {
+            int error = kernel32.GetLastError();
+            log.error("writeMemory:: address = {} , buffer = {} , error = {}", address,buffer, error);
+            return false;
+        }
+
+        return true;
+    }
 }
