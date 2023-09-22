8a02a7100b13662d56d6f861db603be83d7cdef2
qiuapeng <qiuapeng@vchangyi.com>
2023-09-22 16:26:10 +0800

    update

--- src/main/java/com/dnf/game/Base.java
+++ src/main/java/com/dnf/game/Base.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Base {
-    protected Logger logger = LoggerFactory.getLogger(Base.class.getName());
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Resource
     protected ReadWriteMemory memory;
