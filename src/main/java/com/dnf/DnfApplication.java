34ee0e833ca0e6d10c0df1075ed5114da2c7750e
qiuapeng <qiuapeng@vchangyi.com>
2023-10-07 16:30:42 +0800

    update

--- src/main/java/com/dnf/DnfApplication.java
+++ src/main/java/com/dnf/DnfApplication.java
@@ -1,12 +1,14 @@
 package com.dnf;
 
 import com.dnf.game.Initialize;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class DnfApplication {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(DnfApplication.class);
@@ -17,7 +19,7 @@ public class DnfApplication {
 
         // 注册关闭钩子
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.out.println("服务关闭");
+            log.info("服务关闭");
         }));
 
         // 从容器获取实例对象
