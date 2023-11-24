6286e84321b51c2cf1b4f181f196db93b9060431
qiuapeng <qiuapeng@vchangyi.com>
2023-11-24 22:51:01 +0800

    优化代码

--- src/main/java/com/dnf/helper/Process.java
+++ src/main/java/com/dnf/helper/Process.java
@@ -13,26 +13,28 @@ public class Process {
      * @return int
      */
     public static int getProcessId(String processName) {
+        Kernel32 kernel32 = Kernel32.INSTANCE;
+
         Tlhelp32.PROCESSENTRY32.ByReference pe = new Tlhelp32.PROCESSENTRY32.ByReference();
 
         // 创建进程快照
-        WinNT.HANDLE handle = Kernel32.INSTANCE.CreateToolhelp32Snapshot(Tlhelp32.TH32CS_SNAPPROCESS, new WinBase.DWORD(0));
+        WinNT.HANDLE handle = kernel32.CreateToolhelp32Snapshot(Tlhelp32.TH32CS_SNAPPROCESS, new WinBase.DWORD(0));
         if (handle == WinBase.INVALID_HANDLE_VALUE) {
             return 0;
         }
 
         try {
             // 遍历进程快照
-            if (Kernel32.INSTANCE.Process32First(handle, pe)) {
+            if (kernel32.Process32First(handle, pe)) {
                 do {
                     String exeFile = Native.toString(pe.szExeFile);
                     if (exeFile.equalsIgnoreCase(processName.toUpperCase())) {
                         return pe.th32ProcessID.intValue();
                     }
-                } while (Kernel32.INSTANCE.Process32Next(handle, pe));
+                } while (kernel32.Process32Next(handle, pe));
             }
         } finally {
-            Kernel32.INSTANCE.CloseHandle(handle);
+            kernel32.CloseHandle(handle);
         }
 
         return 0;
@@ -46,12 +48,13 @@ public class Process {
      * @return long
      */
     public static long getProcessModuleHandle(int processId, String moduleName) {
-        long result = 0;
+        Kernel32 kernel32 = Kernel32.INSTANCE;
 
-        WinNT.HANDLE hModuleSnap = Kernel32.INSTANCE.CreateToolhelp32Snapshot(Tlhelp32.TH32CS_SNAPMODULE, new WinBase.DWORD(processId));
+        long result = 0;
+        WinNT.HANDLE hModuleSnap = kernel32.CreateToolhelp32Snapshot(Tlhelp32.TH32CS_SNAPMODULE, new WinBase.DWORD(processId));
         Tlhelp32.MODULEENTRY32W me = new Tlhelp32.MODULEENTRY32W();
 
-        if (Kernel32.INSTANCE.Module32FirstW(hModuleSnap, me)) {
+        if (kernel32.Module32FirstW(hModuleSnap, me)) {
             do {
                 String currentProcessModuleName = Native.toString(me.szModule).toLowerCase();
 
@@ -60,10 +63,10 @@ public class Process {
                     result = Pointer.nativeValue(hModule.getPointer());
                     break;
                 }
-            } while (Kernel32.INSTANCE.Module32NextW(hModuleSnap, me));
+            } while (kernel32.Module32NextW(hModuleSnap, me));
         }
 
-        Kernel32.INSTANCE.CloseHandle(hModuleSnap);
+        kernel32.CloseHandle(hModuleSnap);
         return result;
     }
 }
